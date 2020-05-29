b75b448311413dbe88593ef2e418e8e992fded9a
sullay <1181518458@qq.com>
2020-05-29 15:18:18 +0800

    test

---
+++ docs/js/chunk-2d0b68f8.51e2b1c9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b68f8"],{"1e4b":function(n,c,e){"use strict";e.r(c);var t=e("5c40"),s={class:"about"},u=Object(t["j"])("h1",null,"This is an about page",-1);function a(n,c){return Object(t["q"])(),Object(t["g"])("div",s,[u])}const b={};b.render=a;c["default"]=b}}]);
+//# sourceMappingURL=chunk-2d0b68f8.51e2b1c9.js.map
